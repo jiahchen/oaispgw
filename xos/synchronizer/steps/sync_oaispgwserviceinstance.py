0fd6e264c3cda1b11641d2f3e7e2bf5e84f55b99
jiahchen <a7533258@gmail.com>
2018-01-22 10:27:50 +0800

    fix synchronizer

--- xos/synchronizer/steps/sync_oaispgwserviceinstance.py
+++ xos/synchronizer/steps/sync_oaispgwserviceinstance.py
@@ -28,89 +28,20 @@ logger = Logger(level=logging.INFO)
 
 class SyncOAISPGWServiceInstance(SyncInstanceUsingAnsible):
 
-    provides = [OAISPGWServiceInstance]
-
     observes = OAISPGWServiceInstance
-
-    requested_interval = 0
 
     template_name = "oaispgwserviceinstance_playbook.yaml"
 
     service_key_name = "/opt/xos/synchronizers/oaispgw/oaispgw_private_key"
 
-    watches = [ModelLink(ServiceDependency,via='servicedependency'), ModelLink(ServiceMonitoringAgentInfo,via='monitoringagentinfo')]
-
     def __init__(self, *args, **kwargs):
         super(SyncOAISPGWServiceInstance, self).__init__(*args, **kwargs)
-
-    def get_oaispgw(self, o):
-        if not o.owner:
-            return None
-
-        oaispgw = OAISPGWService.objects.filter(id=o.owner.id)
-
-        if not oaispgw:
-            return None
-
-        return oaispgw[0]
 
     # Gets the attributes that are used by the Ansible template but are not
     # part of the set of default attributes.
     def get_extra_attributes(self, o):
         fields = {}
-        fields['tenant_message'] = o.tenant_message
-        """
-        oaispgw = self.get_oaispgw(o)
-
-        for oai in OAISPGWInstance.objects.all():
-            name = oai.tenant_message
-            instance = Instance.objects.filter(id=oai.instance_id).first()
-
-            ip = []
-
-            while not ip:
-                ip = [port.ip for port in instance.ports.all()]
-                time.sleep(2)
-
-            for service, prefix in [('%s_PRIVATE_IP' % name, '10.0'), ('%s_PUBLIC_IP' % name, '10.8')]:
-                service_ip = list(filter(lambda x: x.startswith(prefix), ip))[0]
-                fields[service] = service_ip
-        """
+        #fields['tenant_message'] = o.tenant_message
 
         return fields
 
-    def delete_record(self, port):
-        # Nothing needs to be done to delete an exampleservice; it goes away
-        # when the instance holding the exampleservice is deleted.
-        pass
-
-    def handle_service_monitoringagentinfo_watch_notification(self, monitoring_agent_info):
-        if not monitoring_agent_info.service:
-            logger.info("handle watch notifications for service monitoring agent info...ignoring because service attribute in monitoring agent info:%s is null" % (monitoring_agent_info))
-            return
-
-        if not monitoring_agent_info.target_uri:
-            logger.info("handle watch notifications for service monitoring agent info...ignoring because target_uri attribute in monitoring agent info:%s is null" % (monitoring_agent_info))
-            return
-
-        objs = OAISPGWServiceInstance.objects.all()
-        for obj in objs:
-            if obj.owner.id != monitoring_agent_info.service.id:
-                logger.info("handle watch notifications for service monitoring agent info...ignoring because service attribute in monitoring agent info:%s is not matching" % (monitoring_agent_info))
-                return
-
-            instance = self.get_instance(obj)
-            if not instance:
-               logger.warn("handle watch notifications for service monitoring agent info...: No valid instance found for object %s" % (str(obj)))
-               return
-
-            logger.info("handling watch notification for monitoring agent info:%s for OAISPGWServiceInstance object:%s" % (monitoring_agent_info, obj))
-
-            #Run ansible playbook to update the routing table entries in the instance
-            fields = self.get_ansible_fields(instance)
-            fields["ansible_tag"] =  obj.__class__.__name__ + "_" + str(obj.id) + "_monitoring"
-            fields["target_uri"] = monitoring_agent_info.target_uri
-
-            template_name = "monitoring_agent.yaml"
-            super(SyncOAISPGWServiceInstance, self).run_playbook(obj, fields, template_name)
-        pass
